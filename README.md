3d1ea8c76938ce26dad0ecf5c226b97bb1256fc0
hurielreichel <huriel.ruan@gmail.com>
2020-10-23 12:14:57 +0200

    Add existing file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# swisstopo_interlis_update
+# SWISSTOPO INTERLIS UPDATES
+
+Swisstopo has the INTERLIS dataset of many cantons in Switzerland, though we want to keep track of updates and run change detection algorithms with this data in a spatio-temporal perspective. This code is a simple way of keeping track of the updates being made by swisstopo in the INTERLIS data. 
+
+If there are updates it is able to check on its amount and then write the txt files containing only the links to use with wget. 
+
+Pay close attention to input and output folders, as the idea of *versioning* demands the files not to be overwritten. It is important that the meta.txt file taken from the swisstopo server is also saved in the new backup folder for frequent comparisons.
+
+# WGET
+
+After the txt files are written, simple do the following to download (remember to be in the desired folder with cd):
+
+```
+$ wget -i meta_only_links_TG.txt
+$ wget -i meta_only_links_GE.txt
+``` 
+
+
---
+++ swisstopo_interlis_update.R
@@ -0,0 +1,55 @@
+#  check for updates in swisstopo's INTERLIS
+#
+#     Huriel Reichel - huriel.ruan@gmail.com
+#     Copyright (c) 2020 STDL, Swiss Territorial Data Lab
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# read old meta
+# CHECK INPUT!
+old_meta <- read.table("/media/huriel/Seagate Expansion Drive/temporal_interlis/17_10_2020/meta.txt")
+head(old_meta)
+
+# get only TG and GE data
+old_meta$V3 <- substr(meta$V1,1,nchar(meta$V1)-8)
+old_TG <- old_meta[ which(old_meta$V3 == "https://data.geo.admin.ch/ch.swisstopo-vd.amtliche-vermessung/DM01AVCH24D/SHP/TG/"), ]
+max(old_TG$V2)
+old_GE <- old_meta[ which(old_meta$V3 == "https://data.geo.admin.ch/ch.swisstopo-vd.amtliche-vermessung/DM01AVCH24D/SHP/GE/"), ]
+max(old_GE$V2)
+
+# read new meta
+meta <- read.table(url("https://data.geo.admin.ch/ch.swisstopo-vd.amtliche-vermessung/meta.txt"))
+
+# get only TG and GE data
+meta$V3 <- substr(meta$V1,1,nchar(meta$V1)-8)
+TG <- meta[ which(meta$V3 == "https://data.geo.admin.ch/ch.swisstopo-vd.amtliche-vermessung/DM01AVCH24D/SHP/TG/"), ]
+GE <- meta[ which(meta$V3 == "https://data.geo.admin.ch/ch.swisstopo-vd.amtliche-vermessung/DM01AVCH24D/SHP/GE/"), ]
+
+# Check for updates
+max(TG$V2)
+max(TG$V2)
+
+# If there are updates, subset to get only TG and GE and check for the number of updates
+actual_TG <- meta[ which(meta$V2==max(meta$V2) & meta$V3 == "https://data.geo.admin.ch/ch.swisstopo-vd.amtliche-vermessung/DM01AVCH24D/SHP/TG/"), ]
+actual_GE <- meta[ which(meta$V2==max(meta$V2) & meta$V3 == "https://data.geo.admin.ch/ch.swisstopo-vd.amtliche-vermessung/DM01AVCH24D/SHP/GE/"), ]
+nrow(actual_TG)
+nrow(actual_GE)
+
+# Create links only file
+# CHECK OUTPUT!
+write.table(TG$V1, "/media/huriel/Seagate Expansion Drive/temporal_interlis/23_10_2020/meta_only_links_TG.txt", col.names = F, row.names = F, quote = F)
+write.table(GE$V1, "/media/huriel/Seagate Expansion Drive/temporal_interlis/23_10_2020/meta_only_links_GE.txt", col.names = F, row.names = F, quote = F)
+
+# Save the last updated meta.txt file to future comparison
+write.table(meta[,1:2], "/media/huriel/Seagate Expansion Drive/temporal_interlis/23_10_2020/meta.txt" )
